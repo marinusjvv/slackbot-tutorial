5857c7a80d7a3a6d93f7b593e3de112c29b08906
Marinus Jansen van Vuuren <mvanvuuren@AGENTBOX.local>
2020-09-22 16:59:25 +1000

    Add

--- slashCommand.py
+++ slashCommand.py
@@ -11,6 +11,6 @@ class Slash():
   def message(self, slack_client, info):
     response = slack_client.chat_postMessage(
       channel='#{}'.format(info["channel_name"]),
-      text='Created new channel #temp-channel4',
+      text='sdfdfsdfsdf',
       link_names=1
     )
