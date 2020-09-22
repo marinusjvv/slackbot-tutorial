bcde96218024c889540da3ca589f2aeafc3680e1
Marinus Jansen van Vuuren <mvanvuuren@AGENTBOX.local>
2020-09-22 16:32:35 +1000

    Add

--- server.py
+++ server.py
@@ -29,3 +29,16 @@ def command():
 # Start the Flask server
 if __name__ == "__main__":
   app.run()
+
+# # send user a response via DM
+# im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
+# ownerMsg = slack_client.chat_postMessage(
+#   channel=im_id,
+#   text=commander.getMessage()
+# )
+
+# # send channel a response
+# response = slack_client.chat_postMessage(
+#   channel='#{}'.format(info["channel_name"]),
+#   text=commander.getMessage()
+# )
