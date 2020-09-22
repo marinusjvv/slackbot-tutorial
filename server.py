03f66cae6f95049ed3b42e7d02544ff6fb43adb6
Marinus Jansen van Vuuren <mvanvuuren@AGENTBOX.local>
2020-09-22 16:25:38 +1000

    Add

--- server.py
+++ server.py
@@ -12,23 +12,27 @@ from slashCommand import Slash
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
-logging.debug('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
 
 @app.route("/slack/test", methods=["POST"])
 def command():
-  logging.debug('ZZZZZZZZZZZZZZZZZZZZZZZ')
   SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
   SLACK_SIGNATURE = os.environ['SLACK_SIGNATURE']
 
   verifier = SignatureVerifier(SLACK_SIGNATURE)
-  slack_client = WebClient(SLACK_BOT_TOKEN)
-  info = request.form
-  commander = Slash(verifier, slack_client, info)
-
+  commander = Slash(verifier)
   if not commander.verify(request):
     return make_response("invalid request", 403)
 
 
+  slack_client = WebClient(SLACK_BOT_TOKEN)
+
+
+  logging.debug('YYYYYYYYYYYYYYYYYYYYYYYYYYYYY')
+  logging.debug(__name__)
+  logging.debug('YYYYYYYYYYYYYYYYYYYYYYYYYYYYY')
+  if not verifier.is_valid_request(request.get_data(), request.headers):
+    return make_response("invalid request", 403)
+  info = request.form
 
   # # send user a response via DM
   # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
@@ -39,17 +43,15 @@ def command():
 
   # # send channel a response
   # response = slack_client.chat_postMessage(
-  #   channel='#{}'.format(info["channel_name"]), 
+  #   channel='#{}'.format(info["channel_name"]),
   #   text=commander.getMessage()
   # )
 
   try:
-    logging.debug('XXXXXXXXXXXXXXX')
     response = slack_client.chat_postMessage(
-      channel='#{}'.format(info["channel_name"]), 
+      channel='#{}'.format(info["channel_name"]),
       text='aaaa'
     )#.get()
-    logging.debug('XXXXXXXXXXXXXXX')
   except SlackApiError as e:
     logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
     logging.error(e.response)
--- slashCommand.py
+++ slashCommand.py
@@ -1,40 +1,8 @@
-from pprint import pprint
-
+# basic starter code for a class that can be expanded to handle callbacks, attachents (buttons, etc) and more!
 class Slash():
 
-  def __init__(self, verifier, slack_client, info):
+  def __init__(self, verifier):
     self.verifier = verifier
-    self.slack_client = slack_client
-    self.info = info
 
   def verify(self, request):
     return self.verifier.is_valid_request(request.get_data(), request.headers)
-
-  def process(self):
-    pprint(self.info)
-
-  def send():
-    # # send user a response via DM
-    # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
-    # ownerMsg = slack_client.chat_postMessage(
-    #   channel=im_id,
-    #   text=commander.getMessage()
-    # )
-
-    # # send channel a response
-    # response = slack_client.chat_postMessage(
-    #   channel='#{}'.format(info["channel_name"]),
-    #   text=commander.getMessage()
-    # )
-
-    try:
-      response = self.slack_client.chat_postMessage(
-        channel='#{}'.format(info["channel_name"]),
-        text='aaaa'
-      )#.get()
-    except SlackApiError as e:
-      logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
-      logging.error(e.response)
-      return make_response("", e.response.status_code)
-
-    return make_response("", response.status_code)
